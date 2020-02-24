b2d0a7a581df24cc4776b25ec34a25629d6e8137
Sergey0s <mediaworld@mail.ru>
2020-02-24 12:13:57 +0300

    refactor: change algorithms

--- src/index.js
+++ src/index.js
@@ -1,38 +1,15 @@
 
 exports.min = function min (array) {
-    let min = 0;
-    if (!array || array.length===0) {
-    } else {
-        for (let num of array) {
-            if (num<min) {
-                min = num
-            }
-        }
-    }
-   return min
+    return !array||array.length===0 ? 0 : Math.min(...array)
 };
 
 exports.max = function max (array) {
-   let max = 0;
-    if (!array || array.length===0) {
-    } else {
-        for (let num of array) {
-            if (num>max) {
-                max = num
-            }
-        }
-    }
-    return max
+    return !array||array.length===0 ? 0 : Math.max(...array)
 };
 
 exports.avg = function avg (array) {
-      let avg = 0;
-    if (!array || array.length===0) {
-    } else {
-        for (let num of array) {
-            avg+=num
-        }
-        avg /= array.length
-    }
-    return avg
+    return !array || array.length === 0 ? 0 : array.reduce(
+        function (a, b) {
+            return a+b
+        },0) / array.length
 };
